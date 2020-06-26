695e368893bb4d5e502dbc3951d852213e4cf8ba
w4ms3y <54171091+w4ms3y@users.noreply.github.com>
2020-06-26 17:35:26 +0200

    änderung3

--- experiments/P01-1.R
+++ experiments/P01-1.R
@@ -1,3 +1,19 @@
+test313123 <- "Test"
+test <- "Test"
+  test2 <- "Test"
+test2 <- "Test"
+test2 <- "Test"
+test2 <- "Test"
+
+1:10
+lsq(matrix(letters[1:6] , nrow=2), 1:3)
+lsq(matrix(1:6, nrow=3), list(1,2,3))
+lsq(matrix(1:6, nrow=3), 1:4)
+lsq(matrix(1:6, nrow=3), matrix(1:1, nrow=1))
+lsq(matrix(1:6, nrow=3), matrix(1:1, nrow=3))
+lsq(matrix(double(0), nrow=0, ncol=0), matrix(double(0), nrow=0, ncol=0))
+lsq(matrix(1:6, nrow=3), c(1,NA,3))
+
 lsq <- function(X, y) {
   # TODO
   A <- t(X) %*% X
