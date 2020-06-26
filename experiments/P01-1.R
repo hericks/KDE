74f6879da4f9e99999ca5f38aa51f0b2fcd0236d
w4ms3y <54171091+w4ms3y@users.noreply.github.com>
2020-06-26 17:05:19 +0200

    Aufgabe 1 Blatt 1

---
+++ experiments/P01-1.R
@@ -0,0 +1,19 @@
+lsq <- function(X, y) {
+  # TODO
+  A <- t(X) %*% X
+  solve(A, t(X) %*% y)
+}
+
+lsq(matrix(1:6, nrow=3), 1:3)
+lsq(matrix(runif(6), nrow=3), matrix(runif(3), ncol=1))
+lsq(matrix(letters[1:6] , nrow=2), 1:3)
+lsq(matrix(1:6, nrow=3), list(1,2,3))
+lsq(1:6, 1:3)
+lsq(matrix(1:6, nrow=3), array(1:3, dim=c(1,1,3)))
+lsq(matrix(1:6, nrow=3), 1:4)
+lsq(matrix(1:6, nrow=3), matrix(1:3, nrow=1))
+lsq(matrix(1:6, nrow=3), matrix(1:6, nrow=3))
+lsq(matrix(double(0), nrow=0, ncol=0), matrix(double(0), nrow=0, ncol=0))
+lsq(matrix(1:6, nrow=3), c(1,NA,3))
+lsq(matrix(c(1:5, NA), nrow=3), 1:3)
+lsq(matrix(c(1,1,2,1,1,2), nrow=3), 1:3)
