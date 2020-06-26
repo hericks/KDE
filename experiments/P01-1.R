b35b6c45f7727e1ffc02556fc0f9c982df2a63e2
Matthias Hericks <matthias.hericks@gmail.com>
2020-06-26 17:31:28 +0200

    Test

--- experiments/P01-1.R
+++ experiments/P01-1.R
@@ -7,13 +7,18 @@ lsq <- function(X, y) {
 lsq(matrix(1:6, nrow=3), 1:3)
 lsq(matrix(runif(6), nrow=3), matrix(runif(3), ncol=1))
 lsq(matrix(letters[1:6] , nrow=2), 1:3)
-lsq(matrix(1:6, nrow=3), list(1,2,3))
-lsq(matrix(1:6, nrow=3), 1:4)
-lsq(matrix(1:6, nrow=3), matrix(1:1, nrow=1))
+
+
+lsq(matrix(1:6, nrow=2), matrix(1:1, nrow=1))
 lsq(matrix(1:6, nrow=3), matrix(1:1, nrow=3))
 lsq(matrix(double(0), nrow=0, ncol=0), matrix(double(0), nrow=0, ncol=0))
 lsq(matrix(1:6, nrow=3), c(1,NA,3))
-lsq(matrix(c(1:5, NA), ncol=3), 1:3)
-lsq(matrix(c(1:5, NA), ncol=3), 1:3)
-lsq(matrix(c(1:5, NA), ncol=3), 1:3)
+lsq(matrix(c(1:5, NA), nrow=3), 1:3)
+lsq(matrix(c(1:5, NA), ncol=3), 1:10)
 lsq(matrix(c(1,1,2,1,1,2), nrow=3), 1:3)
+
+lsq <- function(X, y) {
+  # TODO
+  A <- t(X) %*% X
+  solve(A, t(X) %*% y)
+}
