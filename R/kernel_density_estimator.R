f78d963526e18f88e79c814e5d05aa43b9ffcf74
Matthias Hericks <matthias.hericks@gmail.com>
2020-06-28 15:43:28 +0200

    Cleanup after merge conflict.

--- R/kernel_density_estimator.R
+++ R/kernel_density_estimator.R
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-library(rlang)
-
-kde <- function(y, h, kernel){
-  n <- length(y)
-  ker <- eval(expr(!!kernel))
-  function(x){
-    sapply(x,function(x) (1/(n*h))*sum(sapply(y, function(y) ker((x-y)/h))))
-=======
 kernelDensityEstimator <- function(kernel, samples, bandwidth=1) {
   # TODO: Condition checking for arguments
 
@@ -19,6 +10,5 @@ kernelDensityEstimator <- function(kernel, samples, bandwidth=1) {
       ret <- ret + kernel((x - x0)/bandwidth)/bandwidth
     }
     ret/length(samples)
->>>>>>> 7132fe7005b9929b132653fd8db31e6693185551
   }
 }
