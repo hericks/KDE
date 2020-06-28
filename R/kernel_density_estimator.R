aa1d5607fa94f21151cdc4cb5c87fc2f331c9d0a
Matthias Hericks <matthias.hericks@gmail.com>
2020-06-28 18:08:56 +0200

    Minor bug fix with usage of `kernelTransformation()` in `kernelDensityEstimator()`. Moved conditions to top.

--- R/kernel_density_estimator.R
+++ R/kernel_density_estimator.R
@@ -1,23 +1,24 @@
 kernelDensityEstimator <- function(kernel, samples, bandwidth=1) {
-  # TODO: Condition checking for arguments
-  force(kernel)
-  force(samples)
-  force(bandwidth)
-
-  # kernel check
+  # Kernel conditions
   stopifnot("kernel has to satisfy is_kernel() conditions"=is_kernel(kernel))
-  # samples check
+
+  # Samples conditions
   stopifnot(is.numeric(samples))
   stopifnot(length(samples) > 0)
-  # bandwidth check
+
+  # Bandwidth conditions
   stopifnot(is.numeric(bandwidth))
   stopifnot(length(bandwidth) == 1)
   stopifnot(bandwidth > 0)
 
+  force(kernel)
+  force(samples)
+  force(bandwidth)
+
   function(x) {
     ret <- numeric(length(x))
     for (x0 in samples) {
-      ret <- ret + kernelTransform(kernel, x0, bandwidth)
+      ret <- ret + kernelTransform(kernel, x0, bandwidth)(x)
     }
     ret/length(samples)
   }
