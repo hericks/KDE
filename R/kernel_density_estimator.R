3c7c4ba009fdf8f95558d09d62ddd87cc5189342
w4ms3y <54171091+w4ms3y@users.noreply.github.com>
2020-06-28 16:58:53 +0200

    conditions kDE added

--- R/kernel_density_estimator.R
+++ R/kernel_density_estimator.R
@@ -1,8 +1,18 @@
 kernelDensityEstimator <- function(kernel, samples, bandwidth=1) {
   # TODO: Condition checking for arguments
-
   force(kernel)
   force(samples)
+  force(bandwidth)
+
+  # kernel check
+  stopifnot("kernel has to satisfy is_kernel() conditions"=is_kernel(kernel))
+  # samples check
+  stopifnot(is.numeric(samples))
+  stopifnot(length(samples) > 0)
+  # bandwidth
+  stopifnot(is.numeric(bandwidth))
+  stopifnot(length(bandwidth) == 1)
+  stopifnot(bandwidth > 0)
 
   function(x) {
     ret <- numeric(length(x))
@@ -12,3 +22,4 @@ kernelDensityEstimator <- function(kernel, samples, bandwidth=1) {
     ret/length(samples)
   }
 }
+
