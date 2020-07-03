e5b6a1dc023dcc3bf7a0d48aae2d46b5bd310c62
Celine Fietz <celinefietz@gmx.de>
2020-07-03 17:52:35 +0200

    added checks on the arguments

--- R/rejection_sampling.R
+++ R/rejection_sampling.R
@@ -1,10 +1,35 @@
+# tidyverse style guide: all comments are awnsering "why" not "what" or "how"
+
 rejection_sampling <- function(f_den, g_den, g, M) {
   # TODO: Check sufficient conditions on arguments
-
   force(f_den)
   force(g_den)
   force(g)
   force(M)
+
+  # M is constant in the real numbers
+  stopifnot(is.numeric(M))
+
+  ## f_den, g_den should be probability density functions
+  # f_den, g_den are nonnegative
+  stopifnot(class(f_den) == "function")
+  neg_f <- Vectorize(function(x) max(0, -f_den(x)))
+  neg_integral_f <- integrate(neg_f, -Inf, Inf)[[1]]
+  stopifnot(neg_integral_f == 0)
+
+  stopifnot(class(g_den) == "function")
+  neg_g <- Vectorize(function(x) max(0, -g_den(x)))
+  neg_integral_g <- integrate(neg_g, -Inf, Inf)[[1]]
+  stopifnot(neg_integral_g == 0)
+
+  #f_den, g_den are normalized
+  pos_f <- Vectorize(function(x) max(0, f_den(x)))
+  pos_integral_f <- integrate(pos_f, -Inf, Inf)[[1]]
+  stopifnot(pos_integral_f == 1)
+
+  pos_g <- Vectorize(function(x) max(0, g_den(x)))
+  pos_integral_g <- integrate(pos_g, -Inf, Inf)[[1]]
+  stopifnot(pos_integral_g == 1)
 
   function(n) {
     # TODO: Check sufficient conditions on arguments
