32121acbd95156349975ded3bd497694b830dc28
jboerschel <54171091+w4ms3y@users.noreply.github.com>
2020-07-08 13:29:44 +0200

    bugs fixed test-kernel

--- tests/testthat/test-kernel.R
+++ tests/testthat/test-kernel.R
@@ -1,36 +1,50 @@
-  test_that("a kernel should be recognized as a kernel",
-          expect_true(is_kernel(gaussian))
-)
+test_that("a kernel should be built correctly",{
+  rectangular_function <- function(u){
+    return(1/2 * (abs(u) <= 1))
+  }
+  ker <- Kernel(rectangular_function,c(-1,1))
+  expect_equal(ker$fun, rectangular_function)
+  expect_equal(ker$support, c(-1,1))
 
+  # find_borders has to work on rectangular_function
+  ker <- Kernel(rectangular_function, NULL)
+  expect_equal(ker$fun, rectangular_function)
+  # based on how find_borders works, it will find c(-10,10) as borders
+  expect_equal(ker$support, c(-10,10))
 
-test_that("a transformed kernel is still a kernel",{
-  transformed_kernel <- kernelTransform(gaussian, 2, 2)
-  # integrate does not integrate perfectly -> deviation from 1 < integration error
-  transformed_kernel2 <- kernelTransform(rectangular, 0, 0.01)
-  # function with a. e. zero will crash
-  a_e_zero <- kernelTransform(rectangular, 0, 0.00001)
+  # the find_borders has to work on small bandwidth
+  rectangular_function_a_e_zero <- function(u){
+    return(1/2 * 1000 * (abs(u/0.001) <= 1))
+  }
+  ker <- Kernel(rectangular_function_a_e_zero,NULL)
+  expect_equal(0, integrate(rectangular_function_a_e_zero, -1, 1)[[1]])
+  expect_equal(ker, Kernel(rectangular_function_a_e_zero,c(-0.01,0.01)))
+})
 
-  expect_true(is_kernel(transformed_kernel,center=2))
-  expect_true(is_kernel(transformed_kernel2))
-  expect_true(is_kernel(a_e_zero))
-}
-)
+test_that("kernel has to hold a valid support", {
+  rectangular_function <- function(u){
+    return(1/2 * (abs(u) <= 1))
+  }
+  expect_error(Kernel(rectangular_function, c(0.5,0.5)))
+  expect_error(Kernel(rectangular_function, c(0.5,-0.5)))
+  expect_error(Kernel(rectangular_function, c(-0.5,0.5)))
+  expect_error(Kernel(rectangular_function, c("3", "1")))
+  expect_error(Kernel(rectangular_function, 1))
+  expect_error(Kernel(rectangular_function, 1:3))
+})
 
 test_that("the integral of a kernel over the real numbers equals one",{
   div_integral_fun <- function(x) 1
   finite_integral_fun <- function(x) 1*(x <= 100)
   non_integrable_fun <- function(x) 1/x
 
-  expect_false(is_kernel(div_integral_fun))
-  expect_false(is_kernel(finite_integral_fun))
-  expect_false(is_kernel(non_integrable_fun))
-}
-)
+  expect_error(Kernel(div_integral_fun,c(-Inf,Inf)))
+  expect_error(Kernel(finite_integral_fun,c(-Inf,Inf)))
+  expect_error(Kernel(non_integrable_fun,c(-Inf,Inf)))
+})
 
-test_that("kernel is a numeric function",{
+test_that("kernel function has to be a numeric function",{
   non_numeric_fun <- function(x){"x"}
 
-  expect_false(is_kernel("x"))
-  expect_false(is_kernel(non_numeric_fun))
-}
-)
+  expect_error(Kernel(non_numeric_fun,c(-1,1)))
+})
