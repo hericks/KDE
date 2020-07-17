c8398e3c31b8e548a21e550790bd574a8ea3fecd
Matthias Hericks <matthias.hericks@gmail.com>
2020-07-17 13:06:42 +0200

    Update experiment to reflect changes in kernelDensityEstimator return-value.

--- experiments/exp-kernel_density_estimator.R
+++ experiments/exp-kernel_density_estimator.R
@@ -4,25 +4,28 @@ bandwidth <- 0.05
 kernel <- gaussian
 
 # Custom density
-f_den <- function(x) {
+f_den_eval <- function(x) {
   ret <- 1 + sin(2*pi*x)
   ret[x < 0 | 1 < x] <- 0
   ret
 }
 
+f_den <- Density(f_den_eval, c(0,1))
+g_den <- Density(dunif, c(0,1))
+
 # Create sampler from custom density
-custom_sampler <- rejection_sampling(f_den, dunif, runif, 2)
+custom_sampler <- rejection_sampling(f_den, g_den, runif, 2)
 
 # Sample from custom sampler
 samples <- custom_sampler(num_samples)
 
 # Create KDE
-p_hat <- kernelDensityEstimator(kernel, samples, bandwidth)
+p_hat <- kernelDensityEstimator(kernel, samples, bandwidth)$fun
 
 # Plot the density in red
 x <- seq(-3, 3, by=0.005)
 y <- p_hat(x)
-plot(x, f_den(x), col="red", type="l", ylim=c(0,2.5), xlim=c(-0.5,1.5))
+plot(x, f_den_eval(x), col="red", type="l", ylim=c(0,2.5), xlim=c(-0.5,1.5))
 
 # Plot the samples in blue
 points(samples, integer(length(samples)), pch=".", col="blue")
