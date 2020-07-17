9a443f5e088a92ba9fd3c3e42f6ec72c966e99ec
Matthias Hericks <matthias.hericks@gmail.com>
2020-07-17 12:18:49 +0200

    Updated documentation (with example) to reflect the change in return value.

--- R/kernel_transform.R
+++ R/kernel_transform.R
@@ -6,21 +6,23 @@
 #' @param kernel a kernel object.
 #' @param sample the observation/coefficient as a numerical scalar.
 #' @param h the bandwidth as a numerical scalar.
-#' @return the transformed, i.e. shifted and scaled kernel function.
+#'
+#' @return The transformed (shifted and scaled) kernel as valid S3 object of
+#'   class \code{Kernel}.
 #'
 #' @examples
 #' # shifting the gaussian kernel
-#' shifted_gaussian <- kernelTransform(gaussian, 2, 1)
+#' shifted_gaussian <- kernel_transform(gaussian, 2, 1)
 #' # stretching the gaussian kernel
-#' stretched_gaussian <- kernelTransform(gaussian, 0, 2)
+#' stretched_gaussian <- kernel_transform(gaussian, 0, 2)
 #' x <- seq(from = -5, to = 5, length.out = 1000)
 #'
 #' plot(x, gaussian$fun(x),
 #'      xlim=c(-5,5), ylim=c(0,1),
 #'      main="Kernels", xlab="", ylab="",
 #'      col="black", type="l")
-#' lines(x, shifted_gaussian(x), col="red")
-#' lines(x, stretched_gaussian(x), col="blue")
+#' lines(x, shifted_gaussian$fun(x), col="red")
+#' lines(x, stretched_gaussian$fun(x), col="blue")
 #' legend("topright",
 #'        legend=c("gaussian", "shifted_gaussian","stretched_gaussian"),
 #'        col=c("black","red", "blue"), lty=1, cex=0.8)
