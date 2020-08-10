399b40042989f415a1a7485eb1213d641c0b1ac6
jboerschel <54171091+w4ms3y@users.noreply.github.com>
2020-08-10 13:48:02 +0200

    shiny app bandwidth functionalities

--- R/shiny_app.R
+++ R/shiny_app.R
@@ -7,8 +7,10 @@ withConsoleRedirect <- function(containerId, expr) {
   # (messages, warnings, and errors) instead of stdout.
   txt <- capture.output(results <- expr, type = "output")
   if (length(txt) > 0) {
-    insertUI(paste0("#", containerId), where = "beforeEnd",
-             ui = paste0(txt, "\n", collapse = "")
+    insertUI(
+      paste0("#", containerId),
+      where = "beforeEnd",
+      ui = paste0(txt, "\n", collapse = "")
     )
   }
   results
@@ -16,113 +18,270 @@ withConsoleRedirect <- function(containerId, expr) {
 # withConsoleRedirect("console", {print(sampler(reactive_density))})
 
 
+
 ui <- fluidPage(
   pre(id = "console"),
   #Density selection
 
 
-  selectInput("density", "Choose a density:", c("Uniform distribution" = "dunif", "Normal distribution" = "dnorm", "Custom Density" = "custom_dens")),
+  selectInput(
+    "density",
+    "Choose a density:",
+    c(
+      "Uniform distribution" = "dunif",
+      "Normal distribution" = "dnorm",
+      "Custom Density" = "custom_dens"
+    )
+  ),
   fluidRow(
-    column(width=2,
-      conditionalPanel(condition = "input.density == 'dunif'", numericInput("dunif_min", "Min", "0"))),
-    column(width=2, offset=0,
-           conditionalPanel(condition = "input.density == 'dunif'", numericInput("dunif_max","Max", "1")))
-           ),
+    column(
+      width = 2,
+      conditionalPanel(condition = "input.density == 'dunif'", numericInput("dunif_min", "Min", "0"))
+    ),
+    column(
+      width = 2,
+      offset = 0,
+      conditionalPanel(condition = "input.density == 'dunif'", numericInput("dunif_max", "Max", "1"))
+    )
+  ),
   fluidRow(
-    column(width=2,
-           conditionalPanel(condition = "input.density == 'dnorm'", numericInput("dnorm_mean","mean", "0"))),
-    column(width=2, offset=0,
-           conditionalPanel(condition = "input.density == 'dnorm'", numericInput("dnorm_sd", "sd", "1")))),
+    column(
+      width = 2,
+      conditionalPanel(condition = "input.density == 'dnorm'", numericInput("dnorm_mean", "mean", "0"))
+    ),
+    column(
+      width = 2,
+      offset = 0,
+      conditionalPanel(condition = "input.density == 'dnorm'", numericInput("dnorm_sd", "sd", "1"))
+    )
+  ),
 
-  conditionalPanel(condition = "input.density == 'custom_dens'", textInput("Custom Density Term", "custom_density_term")),
+  conditionalPanel(
+    condition = "input.density == 'custom_dens'",
+    textInput("Custom Density Term", "custom_density_term")
+  ),
 
 
   # Kernel selection
-  selectInput("kernel", "Choose a kernel:", c("Rectangular" = "rectangular", "Triangular" = "triangular",
-                                              "Epanechnikov" = "epanechnikov", "Biweight" = "biweight", "Triweight" = "triweight",
-                                              "Tricube" = "tricube","Gaussian" = "gaussian", "Cosine" = "cosine",
-                                              "Logistic" = "logistic", "Sigmoid" = "sigmoid" , "Silverman" = "silverman")),
+  selectInput(
+    "kernel",
+    "Choose a kernel:",
+    c(
+      "Biweight" = "biweight",
+      "Cosine" = "cosine",
+      "Epanechnikov" = "epanechnikov",
+      "Gaussian" = "gaussian",
+      "Logistic" = "logistic",
+      "Rectangular" = "rectangular",
+      "Sigmoid" = "sigmoid" ,
+      "Silverman" = "silverman",
+      "Triangular" = "triangular",
+      "Tricube" = "tricube",
+      "Triweight" = "triweight"
+    )
+  ),
+  checkboxInput("show_kernel", "Plot kernel", 0),
 
+
+  sliderInput(
+    "subdivision",
+    "Subdivisions for integration:",
+    100L,
+    1000L,
+    100L,
+    50
+  ),
   # parameter tweaking
   numericInput("num_samples", "Number of samples:", value = 25L),
   sliderInput("bandwidth", "Choose a bandwidth:", 0.001, 1, 0.5, 0.01),
 
-  plotOutput("plot"),
+  fluidRow(
+    column(width = 2,
+           numericInput("xlim_1", "x lower limit:", value = -2L)),
+    column(
+      width = 2,
+      offset = 0,
+      numericInput("xlim_2", "x upper limit:", value = 2L)
+    )
+  ),
+  fluidRow(
+    column(width = 2,
+           numericInput("ylim_1", "y lower limit:", value = -1L)),
+    column(
+      width = 2,
+      offset = 0,
+      numericInput("ylim_2", "y upper limit:", value = 2L)
+    )
+  ),
 
   # bandwidth estimation
-  actionButton("suggestions", "get bandwidth suggestions")
+  actionButton("suggestions", "get bandwidth suggestions"),
+  tableOutput("bandwidth_table"),
+
+  plotOutput("plot")
 )
 
-server <- function(input, output, session){
+server <- function(input, output, session) {
   dens <- reactive({
-    dens_fun <- function(x){switch(input$density,
-                             "dunif" = dunif(x, input$dunif_min, input$dunif_max),
-                              "dnorm" = dnorm(x, input$dnorm_mean, input$dnorm_sd))
-                            }
+    dens_fun <- function(x) {
+      switch(
+        input$density,
+        "dunif" = dunif(x, input$dunif_min, input$dunif_max),
+        "dnorm" = dnorm(x, input$dnorm_mean, input$dnorm_sd)
+      )
+    }
   })
 
-  dens_fun <- function(x){switch(input$density,
-                             "dunif" = dunif(x, input$dunif_min, input$dunif_max),
-                             "dnorm" = dnorm(x, input$dnorm_mean, input$dnorm_sd))}
+  dens_fun <- function(x) {
+    switch(
+      input$density,
+      "dunif" = dunif(x, input$dunif_min, input$dunif_max),
+      "dnorm" = dnorm(x, input$dnorm_mean, input$dnorm_sd)
+    )
+  }
 
   reactive_density <- reactiveValues()
-  reactive_density$support <- function(){switch(input$density,
-                                                 "dunif" = c(input$dunif_min, input$dunif_max),
-                                                 "dnorm" = NULL)}
-  reactive_density$fun <- function(x){switch(input$density,
-                                             "dunif" = dunif(x, input$dunif_min, input$dunif_max),
-                                             "dnorm" = dnorm(x, input$dnorm_mean, input$dnorm_sd))}
-  reactive_density$object <- function(){Density(reactive_density$fun, reactive_density$support())}
+  reactive_density$support <- function() {
+    switch(
+      input$density,
+      "dunif" = c(input$dunif_min, input$dunif_max),
+      "dnorm" = NULL
+    )
+  }
+  reactive_density$fun <- function(x) {
+    switch(
+      input$density,
+      "dunif" = dunif(x, input$dunif_min, input$dunif_max),
+      "dnorm" = dnorm(x, input$dnorm_mean, input$dnorm_sd)
+    )
+  }
+  reactive_density$object <-
+    function() {
+      Density(reactive_density$fun, reactive_density$support())
+    }
 
 
   reactive_samples <- reactiveValues()
-  reactive_samples$values <- function(){
-    sampler_fun <- function(x){switch(input$density,
-                          "dunif" = runif(x, input$dunif_min, input$dunif_max),
-                          "dnorm" = rnorm(x, input$dnorm_mean, input$dnorm_sd))}
-    sampler <- rejection_sampling(reactive_density$object(), reactive_density$object(),sampler_fun, 1)
-    sampler(input$num_samples)
+  reactive_samples$values <- function() {
+    num_of_samples <- input$num_samples
+    if (is.na(input$num_samples)) {
+      num_of_samples <- 1L
+    }
+    sampler_fun <- function(x) {
+      switch(
+        input$density,
+        "dunif" = runif(x, input$dunif_min, input$dunif_max),
+        "dnorm" = rnorm(x, input$dnorm_mean, input$dnorm_sd)
+      )
+    }
+    sampler <-
+      rejection_sampling(reactive_density$object(),
+                         reactive_density$object(),
+                         sampler_fun,
+                         1)
+    sampler(num_of_samples)
   }
 
-
   reactive_kernel <- reactiveValues()
-  reactive_kernel$object <- function(){switch(input$kernel,
-                                               "rectangular" = rectangular,
-                                               "triangular" = triangular,
-                                              "epanechnikov" = epanechnikov,
-                                              "biweight" = biweight,
-                                              "triweight" = triweight,
-                                              "tricube" = tricube,
-                                              "gaussian" = gaussian,
-                                              "cosine" = cosine,
-                                              "logistic" = logistic,
-                                              "sigmoid" = sigmoid,
-                                              "silverman" = silverman)}
-  reactive_kernel$fun <- function(x){reactive_kernel$object()$fun(x)}
+  reactive_kernel$object <- function() {
+    switch(
+      input$kernel,
+      "rectangular" = rectangular,
+      "triangular" = triangular,
+      "epanechnikov" = epanechnikov,
+      "biweight" = biweight,
+      "triweight" = triweight,
+      "tricube" = tricube,
+      "gaussian" = gaussian,
+      "cosine" = cosine,
+      "logistic" = logistic,
+      "sigmoid" = sigmoid,
+      "silverman" = silverman
+    )
+  }
+  reactive_kernel$fun <-
+    function(x) {
+      reactive_kernel$object()$fun(x)
+    }
 
 
   reactive_kde <- reactiveValues()
-  reactive_kde$kde <- function(){kernel_density_estimator(reactive_kernel$object(), reactive_samples$values(), input$bandwidth)}
-  reactive_kde$fun <- function(x){kernel_density_estimator(reactive_kernel$object(), reactive_samples$values(), input$bandwidth)$fun(x)}
+  reactive_kde$kde <-
+    function(samples, bandwidth) {
+      kernel_density_estimator(reactive_kernel$object(), samples, bandwidth)
+    }
+  reactive_kde$fun <-
+    function(x, samples, bandwidth) {
+      kernel_density_estimator(reactive_kernel$object(), samples, bandwidth)$fun(x)
+    }
 
-  x_lims <- c(-10,10)
-  y_lims <- c(-1,3)
-  x_grid <- seq(from=x_lims[1], to=x_lims[2], length.out=1000)
-
-
-
-
-  output$plot <- renderPlot({
-    plot(x_grid, reactive_density$fun(x_grid))
-    lines(x_grid, reactive_kernel$fun(x_grid))
-    lines(x_grid, reactive_kde$fun(x_grid))
+  reactive_kde$pco <- function(samples) {
+    pco_method(reactive_kernel$object(), samples)
   }
-  )
 
+
+  x_grid <-
+    reactive({
+      seq(
+        from = input$xlim_1,
+        to = input$xlim_2,
+        length.out = 1000
+      )
+    })
+  observeEvent(input$density, {
+    observeEvent(input$num_samples, {
+      samples <- isolate(reactive_samples$values())
+      observeEvent(input$show_kernel, {
+        observeEvent(input$show_kernel, {
+          output$plot <- renderPlot({
+            plot(
+              x_grid(),
+              reactive_density$fun(x_grid()),
+              xlim = c(input$xlim_1, input$xlim_2),
+              ylim = c(input$ylim_1, input$ylim_2)
+            )
+            if (input$show_kernel) {
+              lines(x_grid(), reactive_kernel$fun(x_grid()))
+            }
+            if (input$suggestions) {
+              # bandwidth estimations
+              pco_suggestion <- reactive_kde$pco(samples)
+              #TODO: cv and goldenshluger algorithms!!
+              cv_suggestion <- 1
+              gl_suggestion <- 1
+
+              lines(x_grid(),
+                    reactive_kde$fun(x_grid(), samples, input$bandwidth))
+              lines(x_grid(),
+                    reactive_kde$fun(x_grid(), samples, pco_suggestion))
+              lines(x_grid(),
+                    reactive_kde$fun(x_grid(), samples, cv_suggestion))
+              lines(x_grid(),
+                    reactive_kde$fun(x_grid(), samples, gl_suggestion))
+
+
+              bandwidth_tb <-
+                data.frame(
+                  "pco_method" = pco_suggestion,
+                  "crossvalidation_method" = cv_suggestion,
+                  "goldenshluger_lepski_method" = gl_suggestion
+                )
+              output$bandwidth_table <- renderTable(bandwidth_tb)
+            }
+            else {
+              lines(x_grid(),
+                    reactive_kde$fun(x_grid(), samples, input$bandwidth))
+              points(samples,
+                     integer(length(samples)),
+                     pch = ".",
+                     col = "blue")
+            }
+          })
+        })
+      })
+    })
+  })
 }
 
 shinyApp(ui, server)
-
-
-
 
