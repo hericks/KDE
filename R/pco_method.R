201ba8f2df57b90625f8762d59419025cbce4650
Matthias Hericks <matthias.hericks@gmail.com>
2020-08-16 16:11:30 +0200

    Updated documentation.

--- R/pco_method.R
+++ R/pco_method.R
@@ -1,52 +1,71 @@
-penalty_term <- function(kernel, samples, h_min, h, lambda, subdivisions = 1000L) {
-  n <- length(samples)
-  ker_h_min <- kernel_transform(kernel, 0, h_min, subdivisions)
-  ker_h <- kernel_transform(kernel, 0, h, subdivisions)
-
-  # calculate the variance term
-  l2_h_kernel <-
-    integrate_primitive(function(x) {
-      ker_h$fun(x) ^ 2
-    },
-    lower = ker_h$support[1],
-    upper = ker_h$support[2], subdivisions=subdivisions)$value
-  h_var_term <- lambda * l2_h_kernel / n
-
-
-  z <- integrate_primitive(
-    function(x) {
-      (ker_h_min$fun(x) - ker_h$fun(x)) ^ 2
-    },
-    lower = min(ker_h_min$support[1], ker_h$support[1]),
-    upper = max(ker_h_min$support[2], ker_h$support[2]),
-    subdivisions = subdivisions
-  )$value
-
-  bias_term_pre <- z
-  bias_term <- bias_term_pre / n
-
-  penalty <- h_var_term - bias_term
-}
-
-
-#' Penalized Comparison To Overfitting criterion calculation
-#' @description pco_crit calculates the associated risk for each bandwidth in a given bandwidth set.
+#' Penalized Comparison to Overfitting
 #'
-#' @param kernel kernel function as an S3 object of the class \link[KDE:Kernel]{Kernel}.
-#' @param samples A numerical vector of observations to base the construction of the estimator.
-#' @param bandwidths The bandwidth set from which the bandwidth with the least risk according to the PCO criterion will be derived. The pco_methdod function will try to set up a suitable bandwidth set if \code{NULL} is passed.
-#' @param lambda A tuning parameter. It has to be a numerical value with length 1. The criterion blows up for lambda < 0, therefore the optimal value for lambda is a positiv real number. The recommendation is to set lambda = 1.
-#' @param subdivisions A integer vector of length 1 used for the subdivisions parameter of the builtin R-function \code{\link[stats:integrate]{integrate}}.
+#' @description The PCO method is used to estimate an optimal bandwidth for
+#'   kernel density estimation from a given set of bandwidths.
 #'
-#' @details For more information about the Penalized Comparison to Overfitting method, see [pco_method].
+#' @param kernel S3 object of class \code{\link{Kernel}}; the kernel to use for
+#'   the estimator
+#' @param samples numeric vector; the observations.
+#' @param bandwidths strictly positive numeric vector; the bandwidth set from
+#'   which the bandwidth with the least estimated risk will be selected.
+#' @param lambda positive numeric scalar; a tuning parameter.
+#' @param subdivisions positive numeric scalar; subdivisions parameter
+#'   internally passed to \code{\link{integrate_primitive}}.
 #'
-#' @return A list containing the bandwidth set and associated risk values according to the PCO criterion.
+#' @return The estimated optimal bandwidth contained in the bandwidth set.
 #'
-#' @seealso \code{\link[KDE:pco_method]{pco_method}} for more information about the PCO method.
+#' @details The PCO method aims to minimize an upper bound for the mean
+#'   integrated squared error (MISE) of a kernel density estimator. The MISE is
+#'   defined as the expectation of the squared L2-Norm of the difference between
+#'   estimator and (unknown) true density.
 #'
-#'#' @include kernel.R
+#'   \code{pco_method} internally uses a criterion function to calculate the PCO
+#'   criterion value, approximating the risk. Subsequently the bandwidth with
+#'   the minimal criterion value is selected.
+#'
+#'   The popular bias-/variance-decomposition is used. The bias term still
+#'   depends on the unknown density. Thus, a comparison of the estimator with an
+#'   associated bandwidth to the overfitting one, namely the estimator with the
+#'   smallest bandwidth, is used to estimate the bias term itself.
+#'
+#'   Further a penalty term is computed as the sum of two variances,
+#'   particularly the variance of the risk decomposition and the variance of the
+#'   bias term estimation. During the calculation the tuning parameter
+#'   \code{lambda} is used. The recommended value for \code{lambda} is 1.
+#'
+#'   The PCO criterion is given by the sum of the comparison to overfitting and
+#'   the penalty term, thus the procedure tries to find a balance between those
+#'   terms. Therefore, it is comprehensible why this method is called penalized
+#'   comparison to overfitting.
+#'
+#'   For more information see the linked papers below.
+#'
+#' @seealso \code{\link{kernel_density_estimator}} for more information about
+#'   kernel density estimators, \code{\link{cross_validation}} and
+#'   \code{\link{goldenshluger_lepski}} for more automatic bandwidth-selection
+#'   algorithms.
+#'
+#' @source \href{https://arxiv.org/abs/1607.05091v2}{Estimator selection: a new
+#'   method with applications to KDEs}, Lacour \[2017\]
+#' @source \href{https://arxiv.org/abs/1902.01075}{Numerical performance of
+#'   PCO for multivariate KDEs}, Varet \[2019\]
+#'
+#' @include kernel.R
+#' @include kernel_density_estimator.R
+#' @include logarithmic_bandwidth_set.R
 #'
 #' @export
+pco_method <- function(kernel,
+                       samples,
+                       bandwidths = logarithmic_bandwidth_set(1/length(samples), 1, 10),
+                       lambda = 1,
+                       subdivisions = 100L) {
+
+  # Argchecks are being made in pco_crit, hence the pco_crit function can be used without pco_method, but not the other way around.
+  res_tuple <- pco_crit(kernel, samples, bandwidths, lambda, subdivisions)
+  h_est <- bandwidths[which.min(res_tuple$risk)]
+}
+
 pco_crit <- function(kernel, samples, bandwidths = logarithmic_bandwidth_set(1/length(samples), 1, 10), lambda = 1, subdivisions = 100L) {
   # conditions for kernel
   tryCatch({
@@ -110,65 +129,32 @@ pco_crit <- function(kernel, samples, bandwidths = logarithmic_bandwidth_set(1/l
   list(bandwidth_set = bandwidths, risk = res)
 }
 
+penalty_term <- function(kernel, samples, h_min, h, lambda, subdivisions = 1000L) {
+  n <- length(samples)
+  ker_h_min <- kernel_transform(kernel, 0, h_min, subdivisions)
+  ker_h <- kernel_transform(kernel, 0, h, subdivisions)
 
-#' Penalized Comparison to Overfitting
-#'
-#' @description The PCO method is used to estimate a optimal bandwidth for kernel density estimation from a given set of bandwidths.
-#'
-#' @param kernel kernel function as an S3 object of the class \link[KDE:Kernel]{Kernel}.
-#' @param samples A numerical vector of observations.
-#' @param bandwidths The bandwidth set from which the bandwidth with the least risk according to the PCO criterion will be derived. The \code{pco_method} function will try to set up a suitable bandwidth set using \code{\link[KDE:logarthmic_bandwidth_set]{logarithmic_bandwidth_set}} if \code{NULL} is passed.
-#' @param lambda A tuning parameter. It has to be a numerical value with length 1. The criterion blows up for lambda < 0, therefore the optimal value for lambda is a positiv real number. The recommendation is to set lambda = 1.
-#' @param subdivisions A integer vector of length 1 used for the subdivisions parameter of the builtin R-function \code{\link[stats:integrate]{integrate}}.
-#'
-#' @return A numerical vector of length 1 containing the bandwidth with minimal risk according to the PCO criterion, given by \code{\link[pco_crit]{pco_crit}}.
-#'
-#' @details This method is an implementation of the penalized comparison to overfitting method for bandwith estimation. The aim is to minimize the risk of a kernel density estimator (KDE). \cr
-#' pco_method uses \code{\link[KDE:pco_crit]{pco_crit}} to calculate the PCO criterion value, approximating the risk.
-#' For each bandwidth given in \code{bandwidths}, the method selects the bandwidth with the minimal associated risk.
-#'
-#' The risk function is given by the expected value of the integrated square error of the KDE with
-#' a given bandwith and the desired density that is matching the distibution of the samples (which we
-#'                                                                                           are trying to estimate with the KDE).
-#' By applying this method the risk will be decomposed into a bias and a variance term, where the
-#' bias term needs to be estimated, because it includes a dependency of the real function that we are
-#' trying to estimate with the KDE. The variance term is simply a bound for the variance of the KDE (created with a
-#' bandwith \code{h}), which is tuned by a parameter \code{lambda}.\cr
-#' The bias term will be estimated by a comparison of the KDE of a bandwidth \code{h} to the KDE of the
-#' smallest bandwidth \code{h_min} out of the given bandwidth set \code{bandwidths}. \cr
-#' A penalty term consisting of the sum of the two variance terms is introduced, including the variance from the risk decomposition and the one from
-#' the bias term estimation.
-#' The PCO criterion is given by the sum of the comparison to overfitting and the penalty term.
-#' Now it is comprehensible why this method is called Penalized Comparison to Overfitting. \cr
-#' In the end the method compares the KDE of each bandwidth in \code{bandwidths} to the KDE of the smallest bandwidth out
-#' of the given bandwidth collection. Finally the bandwidth with minimal associated risk will be returned. \cr
-#' For more information see the linked papers below.
-#'
-#'
-#'
-#'
-#' @seealso
-#' \itemize{\code{\link[KDE:pco_crit]{pco_crit}} for getting the associated criterion values.}
-#' \itemize{\code{\link[KDE:goldenshluger_lepski]{Goldenshluger-Lepski method}},
-#' \code{\link[KDE:cross_validation]{Cross-Validation method}} to see other methods for estimating bandwidths.}
-#' \itemize{\code{\link[KDE:Kernel]{Kernel}} to see the definiotion of a kernel.}
-#' \itemize{\code{\link[KDE:kernel_density_estimator]{Kernel Density Estimator}} to see the functionality of the Kernel density estimation.}
-#'
-#' @source \href{https://arxiv.org/abs/1607.05091v2}{Lacour `[`2017`]`}
-#' @source \href{https://arxiv.org/abs/1902.01075}{Varet `[`2019`]`}
-#'
-#' @include kernel.R
-#' @include kernel_density_estimator.R
-#' @include logarithmic_bandwidth_set.R
-#' @export
-pco_method <- function(kernel,
-                       samples,
-                       bandwidths = logarithmic_bandwidth_set(1/length(samples), 1, 10),
-                       lambda = 1,
-                       subdivisions = 100L) {
+  # calculate the variance term
+  l2_h_kernel <-
+    integrate_primitive(function(x) {
+      ker_h$fun(x) ^ 2
+    },
+    lower = ker_h$support[1],
+    upper = ker_h$support[2], subdivisions=subdivisions)$value
+  h_var_term <- lambda * l2_h_kernel / n
 
-  # Argchecks are being made in pco_crit, hence the pco_crit function can be used without pco_method, but not the other way around.
 
-  res_tuple <- pco_crit(kernel, samples, bandwidths, lambda, subdivisions)
-  h_est <- bandwidths[which.min(res_tuple$risk)]
+  z <- integrate_primitive(
+    function(x) {
+      (ker_h_min$fun(x) - ker_h$fun(x)) ^ 2
+    },
+    lower = min(ker_h_min$support[1], ker_h$support[1]),
+    upper = max(ker_h_min$support[2], ker_h$support[2]),
+    subdivisions = subdivisions
+  )$value
+
+  bias_term_pre <- z
+  bias_term <- bias_term_pre / n
+
+  penalty <- h_var_term - bias_term
 }
