8867dde06a1e6543224ae764c8a001b32815dff8
jboerschel <54171091+w4ms3y@users.noreply.github.com>
2020-08-17 17:36:05 +0200

    some code for simulation study

--- R/simulation_study.R
+++ R/simulation_study.R
@@ -19,7 +19,6 @@ compare <- function(eval_points,
     if(length(eval_points) != length(funs) && length(eval_points) > 1) stop("eval_point set has to be of same length as funs")
   }
 
-
   m <- length(funs) * length(ns) * length(kernels)
   m <-
     m * (any(sapply(
@@ -276,3 +275,100 @@ plot_comparison <- function(dens = Density(dunif, c(0, 1)),
     }
   }
 }
+
+library(tidyverse)
+
+compare_ise <- function(dens_list = list(dunif=Density(dunif, c(0, 1))),
+                        dens_sampler_list = list(runif),
+                        bandwidth_estimators = list(cross_validation=cross_validation, goldenshluger_lepski=goldenshluger_lepski, pco_method=pco_method),
+                        ns = 50,
+                        kernels = list(gaussian=gaussian),
+                        lambda_set = list(1),
+                        kappa_set = list(1.2),
+                        reps = 3,
+                        num_eval_points= 30,
+                        length.out_bandwidths = 5){
+
+  eval_points_set <- list()
+  for(d in dens_list){
+    eval_points_range <-  c(d$support[1] - 1, d$support[2] + 1)
+    eval_points <- seq(from=eval_points_range[1],to=eval_points_range[2], length.out=num_eval_points)
+    eval_points <- list(eval_points)
+    eval_points_set <- c(eval_points_set, eval_points)
+  }
+  time <-system.time(res <- compare(eval_points=eval_points_set, funs=dens_sampler_list, ns=ns, kernels=kernels,
+                                    bandwidth_estimators=bandwidth_estimators,
+                                    lambda_set=lambda_set, kappa_set=kappa_set, reps=reps, length.out=length.out_bandwidths))
+  print(time)
+
+  #f_true <- sapply(dens_list, function(d) d$fun(x_grid))
+  f_true <- array(NA, dim=c(length(eval_points_set[[1]]), length(dens_list)))
+  for(i in seq_along(dens_list)){
+    f <- dens_list[[i]]$fun
+    f_true[,i] <- f(eval_points_set[[i]])
+  }
+
+  m <- length(ns) * length(kernels)
+  m <-
+    m * (any(sapply(
+      bandwidth_estimators, identical, cross_validation
+    ))
+    + length(kappa_set) * any(
+      sapply(bandwidth_estimators, identical, goldenshluger_lepski)
+    )
+    + length(lambda_set) * any(sapply(
+      bandwidth_estimators, identical, pco_method
+    )))
+
+  f_true <- f_true[,rep(seq_along(dens_list), each=m)]
+  diff <-array(NA, dim=dim(res))
+
+  for(j in 1:dim(res)[3]) diff[,,j] <- res[,,j] - f_true[,j]
+  ise_pre <-apply(diff^2,c(2, 3), sum)
+  ise <- (ise_pre * (eval_points_range[2] - eval_points_range[1])) / length(eval_points)
+
+
+  if (length(kappa_set) > 1 &&
+      length(lambda_set) > 1) {
+    stop("how is this supposed to look like?")
+  }
+  else if (length(kappa_set) > 1) {
+    opts <-as_tibble(expand.grid(kappa=kappa_set, bandwidth_estimators=bandwidth_estimators, kernel=names(kernels), n=ns, dens=names(dens_list)))
+  }
+  else if (length(lambda_set) > 1) {
+    opts <-as_tibble(expand.grid(lambda=lambda_set, bandwidth_estimators=bandwidth_estimators, kernel=names(kernels), n=ns, dens=names(dens_list)))
+
+  } else {
+    opts <-as_tibble(expand.grid(bandwidth_estimators=names(bandwidth_estimators), kernel=names(kernels), n=ns, dens=names(dens_list)))
+  }
+  add_column(opts[rep(1:nrow(opts), each=reps), ], ise=as.vector(ise))
+}
+
+
+
+
+#bandwidth_estimators=bandwidth_estimators, kernel=names(kernels), n=ns, dens=names(dens_list)
+
+calculate_mise <- function(data_ise){
+  if("kappa" %in% names(data_ise)){
+    group_by(dens, n, kernel, bandwidth_estimators, kappa) %>%
+      summarise(mise = mean(ise), med_ise=median(ise), sd_ise=sd(ise), reps= n()) %>%
+      ungroup() ->
+      data_mise
+
+  }else if("lambda" %in% names(data_ise)){
+    data_ise %>%
+      group_by(dens, n, kernel, bandwidth_estimators, lambda) %>%
+      summarise(mise = mean(ise), med_ise=median(ise), sd_ise=sd(ise), reps= n()) %>%
+      ungroup() ->
+      data_mise
+  } else {
+    data_ise %>%
+      group_by(dens, n, kernel, bandwidth_estimators) %>%
+      summarise(mise = mean(ise), med_ise=median(ise), sd_ise=sd(ise), reps= n()) %>%
+      ungroup() ->
+      data_mise
+  }
+  data_mise
+}
+
--- experiments/exp-simulation_study.R
+++ experiments/exp-simulation_study.R
@@ -1,68 +1,3 @@
-library(tidyverse)
-
-compare_ise <- function(dens_list = list(dunif=Density(dunif, c(0, 1))),
-                        dens_sampler_list = list(runif),
-                        bandwidth_estimators = list(cross_validation=cross_validation, goldenshluger_lepski=goldenshluger_lepski, pco_method=pco_method),
-                        ns = 50,
-                        kernels = list(gaussian=gaussian),
-                        lambda_set = list(1),
-                        kappa_set = list(1.2),
-                        reps = 3,
-                        num_eval_points= 30,
-                        length.out_bandwidths = 5){
-
-  eval_points_set <- list()
-  for(d in dens_list){
-    eval_points_range <-  c(d$support[1] - 1, d$support[2] + 1)
-    eval_points <- seq(from=eval_points_range[1],to=eval_points_range[2], length.out=num_eval_points)
-    eval_points <- list(eval_points)
-    eval_points_set <- c(eval_points_set, eval_points)
-  }
-  time <-system.time(res <- compare(eval_points=eval_points_set, funs=dens_sampler_list, ns=ns, kernels=kernels,
-                                   bandwidth_estimators=bandwidth_estimators,
-                                   lambda_set=lambda_set, kappa_set=kappa_set, reps=reps, length.out=length.out_bandwidths))
-  print(time)
-
-  #f_true <- sapply(dens_list, function(d) d$fun(x_grid))
-  f_true <- array(NA, dim=c(length(eval_points_set[[1]]), length(dens_list)))
-  for(i in seq_along(dens_list)){
-    f <- dens_list[[i]]$fun
-    f_true[,i] <- f(eval_points_set[[i]])
-  }
-
-  m <- length(ns) * length(kernels)
-  m <-
-    m * (any(sapply(
-      bandwidth_estimators, identical, cross_validation
-    ))
-    + length(kappa_set) * any(
-      sapply(bandwidth_estimators, identical, goldenshluger_lepski)
-    )
-    + length(lambda_set) * any(sapply(
-      bandwidth_estimators, identical, pco_method
-    )))
-
-  f_true <- f_true[,rep(seq_along(dens_list), each=m)]
-  diff <-array(NA, dim=dim(res))
-
-  for(j in 1:dim(res)[3]) diff[,,j] <- res[,,j] - f_true[,j]
-  ise <-apply(diff^2,c(2, 3), mean)
-
-  if (length(kappa_set) > 1 &&
-      length(lambda_set) > 1) {
-    stop("how is this supposed to look like?")
-  }
-  else if (length(kappa_set) > 1) {
-    opts <-as_tibble(expand.grid(kappa_set=kappa_set, bandwidth_estimators=bandwidth_estimators, kernel=names(kernels), n=ns, dens=names(dens_list)))
-  }
-  else if (length(lambda_set) > 1) {
-    opts <-as_tibble(expand.grid(lambda_set=lambda_set, bandwidth_estimators=bandwidth_estimators, kernel=names(kernels), n=ns, dens=names(dens_list)))
-
-  } else {
-    opts <-as_tibble(expand.grid(bandwidth_estimators=names(bandwidth_estimators), kernel=names(kernels), n=ns, dens=names(dens_list)))
-  }
-  add_column(opts[rep(1:nrow(opts), each=reps), ], ise=as.vector(ise))
-}
 dens_list <- list(
   uniform_distri=Density(dunif, c(0,1)),
   normal_distri=Density(dnorm, c(-15,15))
@@ -74,16 +9,126 @@ sampler_list <- list(
   normal_distri=rnorm
 )
 
-data_ise <- compare_ise(dens_list, sampler_list)
+#data_ise <- compare_ise(dens_list, sampler_list)
+#mse_data <- calculate_mise(data_ise)
+#mse_data
 
-#bandwidth_estimators=bandwidth_estimators, kernel=names(kernels), n=ns, dens=names(dens_list)
+#code for simulation study vignette
 
-overall_mise <- function(data_ise){
-  if("kappa_set" %in% names(data_ise)){
+# Custom density
+f_dens <- function(x) {
+  ret <- 1 + sin(2*pi*x)
+  ret[x < 0 | 1 < x] <- 0
+  ret
+}
+support_density <- c(0,1)
+# constructing the Density object
+dens <- Density(f_dens, support_density, subdivisions=1000L)
 
-  }else if("lambda_set" %in% names(data_ise)){
+x_lim <- c(dens$support[1] - 1, dens$support[2] + 1)
+grid <- seq(from=x_lim[1], to=x_lim[2], length.out=300)
+# plotting the density
+plot(grid, dens$fun(grid), xlim = x_lim, ylim=c(-1,2),
+     xlab = "",
+     ylab = "",
+     col = "dark red",
+     type = "l",
+     lwd = 2)
 
-  } else {
+# setting up a sampler for the density
 
+# Create sampler from custom density
+g_den <- Density(dunif, c(0,1))
+custom_sampler <- rejection_sampling(dens, g_den, runif, 2)
+
+# Sample from custom sampler
+samples <- list()
+n_samples <- c(10, 50, 1000)
+for(n in n_samples){
+  samples <- c(samples, list(custom_sampler(n)))
+}
+
+kernels <- list(rectangular=rectangular, gaussian=gaussian, epanechnikov=epanechnikov)
+
+for(i in seq_along(kernels)){
+  name <- names(kernels)[[i]]
+  k <- kernels[[i]]
+  par(mfrow=c(1,3))
+  for(s in samples){
+  kde <- kernel_density_estimator(k,s, bandwidth=0.1)
+  plot(grid, dens$fun(grid), xlim = x_lim, ylim=c(-1,2),
+       main=paste(length(s), "samples"),
+       xlab = "",
+       ylab = "",
+       col = "dark red",
+       type = "l",
+       lwd = 2)
+  lines(grid, kde$fun(grid))
   }
 }
+
+# as we can see, as the number of samples increases, the kernel will get more and more irrelevant
+# mathematically we will calculate the MISE to get the empirical proof
+mise_vec <- c()
+reps <- 100
+# TODO: bandweite, die das gut demonstriert wählen (auch in plots)
+for(i in seq_along(kernels)){
+  name <- names(kernels)[[i]]
+  k <- kernels[[i]]
+  for(n in n_samples){
+    print(i)
+    ise_vec <- c()
+    for(rep in 1:reps){
+      samples <- custom_sampler(n)
+      kde <- kernel_density_estimator(k, samples, bandwidth=0.5)
+      ise_vec <- c(ise_vec, (sum((kde$fun(grid) - dens$fun(grid))^2) * (x_lim[2] - x_lim[1])) / length(grid))
+    }
+    mise_vec <- c(mise_vec, mean(ise_vec))
+  }
+}
+
+#TODO: benennen der spalten und reihen
+mise <- matrix(mise_vec, nrow=length(n_samples))
+mise
+
+# -> we will set the kernel to gaussian and work on the largest set of samples
+# the important parameter for a good estimation is the bandwidth
+# above, we did set the bandwidth to h=0.1
+# as you can see, the bandwidth is a much more important parameter than the kernel:
+# TODO: legende und verschiedene Farben für bandweiten
+par(mfrow = c(1,1))
+bandwidth_set <- list(1, 0.5, 0.1, 0.01, 0.001)
+kernel <- gaussian
+n_samples <- 1000
+samples <- custom_sampler(n_samples)
+plot(grid, dens$fun(grid), xlim = x_lim, ylim=c(-1,2),
+     main="bandwidths",
+     xlab = "",
+     ylab = "",
+     col = "dark red",
+     type = "l",
+     lwd = 2)
+for(h in bandwidth_set){
+  kde <- kernel_density_estimator(kernel ,samples, bandwidth=h)
+  lines(grid, kde$fun(grid))
+}
+
+mise_vec <- c()
+reps <- 10
+for(h in bandwidth_set){
+  ise_vec <- c()
+  for(rep in 1:reps){
+    samples <- custom_sampler(n_samples)
+    kde <- kernel_density_estimator(kernel, samples, bandwidth=h)
+    ise_vec <- c(ise_vec, (sum((kde$fun(grid) - dens$fun(grid))^2) * (x_lim[2] - x_lim[1])) / length(grid))
+  }
+  mise_vec <- c(mise_vec, mean(ise_vec))
+}
+#TODO: benennen der spalten und reihen
+mise <- matrix(mise_vec, ncol=length(bandwidth_set))
+print(mise)
+
+
+# TODO: 1. bandweitenschätzer vorstellen (mathematisch?),
+# 2. lambda/kappa wählen
+# 3. Schätzer mit optimalen lambda/kappa
