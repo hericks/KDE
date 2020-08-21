aa2920ccc4c5687c6a678af324d2d72e7b83a7d4
Matthias Hericks <matthias.hericks@gmail.com>
2020-08-21 18:15:44 +0200

    Removed files not belonging into master.

--- experiments/kernel_density_estimator_JM.R
+++
@@ -1,9 +0,0 @@
-library(rlang)
-
-kde <- function(y, h, kernel){
-  n <- length(y)
-  ker <- eval(expr(!!kernel))
-  function(x){
-    sapply(x,function(x) (1/(n*h))*sum(sapply(y, function(y) ker((x-y)/h))))
-  }
-}
